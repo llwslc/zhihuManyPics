b367ca90c4e059b994b905d4186264a7373e8c40
llwslc <llwslc@gmail.com>
2016-04-27 19:25:59 +0800

    update

--- js/demo.js
+++ js/demo.js
@@ -64,6 +64,45 @@
     var xxx = dataUrlToBlob(imgx.src);
     console.log(xxx)
 
+                var format = "image/jpeg";
+                var base64 = base64Data;
+                var code = window.atob(base64.split(",")[1]);
+                var aBuffer = new window.ArrayBuffer(code.length);
+                var uBuffer = new window.Uint8Array(aBuffer);
+                for(var i = 0; i < code.length; i++){
+                    uBuffer[i] = code.charCodeAt(i) & 0xff ;
+                }
+                console.info([aBuffer]);
+                console.info(uBuffer);
+                console.info(uBuffer.buffer);
+                console.info(uBuffer.buffer==aBuffer); //true
+
+                var blob=null;
+                try{
+                    blob = new Blob([uBuffer], {type : format});
+                }
+                catch(e){
+                    window.BlobBuilder = window.BlobBuilder ||
+                    window.WebKitBlobBuilder ||
+                    window.MozBlobBuilder ||
+                    window.MSBlobBuilder;
+                    if(e.name == 'TypeError' && window.BlobBuilder){
+                        var bb = new window.BlobBuilder();
+                        bb.append(uBuffer.buffer);
+                        blob = bb.getBlob("image/jpeg");
+
+                    }
+                    else if(e.name == "InvalidStateError"){
+                        blob = new Blob([aBuffer], {type : format});
+                    }
+                    else{
+
+                    }
+                }
+                alert(blob.size);
+                return blob;
+
+            };
 
 
         document.addEventListener('copy', function(e){
